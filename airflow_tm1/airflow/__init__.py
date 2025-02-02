3b8701ff1ee548b71aa51931ebba9a22aec0a3b6
Joe Chow HK <164451309+Cubewise-JoeCHK@users.noreply.github.com>
2025-02-02 19:03:53 +0800

    5 feat turbointegrator trigger airflow dags (#14)
    * feat: refactor TM1 connection handling and update callback functions for improved configuration management
    
    * feat: implement Airflow DAG execution with dynamic configuration and variable updates

--- airflow_tm1/airflow/__init__.py
+++ airflow_tm1/airflow/__init__.py
@@ -1,7 +1,11 @@
 from datetime import timedelta
 
+from airflow_tm1.utils.config import ProjectConfig
+
 default_args = {
     "owner": "Joe",
     "retries": 1,
     "retry_delay": timedelta(minutes=5),
 }
+
+ProjectConfig.load_config_from_airflow()
--- airflow_tm1/utils/tm1.py
+++ airflow_tm1/utils/tm1.py
@@ -1,5 +1,7 @@
 from TM1py import TM1Service
 
+from airflow_tm1.utils.config import ProjectConfig
 
-def connect_to_tm1(instance_config):
-    return TM1Service(**instance_config)
+
+def connect_to_tm1(instance_name: str = ""):
+    return TM1Service(**ProjectConfig.get_tm1_instance_config(instance_name))
